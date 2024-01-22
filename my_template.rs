a18b8b4b6c8b4a93aa407c245c1f23826644df02
Stepfen Shawn <m18824909883@163.com>
2024-01-23 00:04:16 +0800

    Update and rename io_template.rs to my_template.rs

---
+++ my_template.rs
@@ -0,0 +1,47 @@
+#[allow(unused_imports)]
+use std::cmp::{min,max};
+use std::io::{BufWriter, stdin, stdout, Write};
+const BITS: usize = 19;
+ 
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+#[allow(unused_variables)]
+macro_rules! io_init {
+    ($scan: ident, $out: ident) => {
+        let mut $scan: Scanner = Scanner::default();
+        let $out = &mut BufWriter::new(stdout());
+    };
+}
+
+macro_rules! array_2d {
+    ($n: expr, $m: expr) => {
+        vec![vec![0; $m]; $n]
+    }
+}
+
+fn solve() {
+    // ...
+}
+
+fn main() {
+    io_init!(scan, out);
+    let t = scan.next();
+    for _ in 0..t {
+        solve();
+    }
+}
