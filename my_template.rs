131ea2f25d584374cb3a08fca6db36aa12012f46
Stepfen Shawn <m18824909883@163.com>
2024-02-08 00:01:35 +0800

    Add readline

--- my_template.rs
+++ my_template.rs
@@ -1,22 +1,41 @@
 #[allow(unused_imports)]
 use std::cmp::{min,max};
 use std::io::{BufWriter, stdin, stdout, Write};
-const BITS: usize = 19;
  
 #[derive(Default)]
 struct Scanner {
     buffer: Vec<String>
 }
+
+#[warn(dead_code)]
+enum Split {
+    WHITESPACE,
+    NEWLINE
+}
+
 impl Scanner {
-    fn next<T: std::str::FromStr>(&mut self) -> T {
+    fn read<T: std::str::FromStr>(&mut self, split_ty: Split) -> T {
         loop {
             if let Some(token) = self.buffer.pop() {
                 return token.parse().ok().expect("Failed parse");
             }
             let mut input = String::new();
             stdin().read_line(&mut input).expect("Failed read");
-            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+            match split_ty {
+                Split::WHITESPACE =>
+                    self.buffer = input.split_whitespace().rev().map(String::from).collect(),
+                Split::NEWLINE =>
+                    self.buffer = input.split_inclusive("\n").rev().map(String::from).collect()
+            }
         }
+    }
+
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.read::<T>(Split::WHITESPACE)
+    }
+
+    pub fn next_line<T: std::str::FromStr>(&mut self) -> T {
+        self.read::<T>(Split::NEWLINE)
     }
 }
 
@@ -28,10 +47,15 @@ macro_rules! io_init {
     };
 }
 
-macro_rules! input {
+macro_rules! read {
     ($scan: ident, $($v: pat => $t: ty), *) => {
-        let mut scan = $scan;
-        $(let $v = scan.next::<$t>();)*
+        $(let $v = $scan.next::<$t>();)*
+    };
+}
+
+macro_rules! read_line {
+    ($scan: ident, $($v: pat => $t: ty), *) => {
+        $(let $v = $scan.next_line::<$t>();)*
     };
 }
 
